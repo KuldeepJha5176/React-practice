bf0b271af617970c9bad3c2f41ec517e4360644b
KuldeepJha5176 <ranacjha@gmail.com>
2024-10-08 22:11:13 +0530

    learned about contional rendering

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 create the first basic application of counter
+<br>
 created stopwatch
+<br>
+conditional rendering learned
--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,25 @@ import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
+  let [countervisible, setCountervisible]  = useState(true); //conditional rendering example on counter component
+
+  useEffect(function() {
+    setInterval(function() {
+        setCountervisible(c => !c)//component counter will render after every 5 second
+    },5000);
+  }, [])
+
   return (
-    <Counter></Counter>
-)
+    <div>
+   {countervisible && <Counter></Counter> }
+   </div>
+  )
+  
 }
 function Counter() {
   const [count, setCount] = useState(0);
   useEffect(function(){
-      setInterval(function () {   //here whenever the state changes react re-rendering the counter component
+      setInterval(function () {   
         setCount(count => count + 1 );
       }, 1000)
       console.log("mounted"); 
